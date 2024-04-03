d08686b75dbed98510da3d6aa7eab873b3768099
Gabriela Rappel <gabrielafr3005@gmail.com>
2024-04-03 14:54:24 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'src/calc_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      themeMode: ThemeMode.dark,
+      darkTheme: ThemeData.dark(
+        useMaterial3: true,
+      ),
+      home: const CalcPage(),
+    );
+  }
+}
+
